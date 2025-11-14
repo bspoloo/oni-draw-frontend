1990f2c2f4ef35a30a72fe4aba5c9d7519ef84b4
bspoloo <animetx098@gmail.com>
2025-11-14 04:11:24 -0400

    añadido arreglo en formulario

---
+++ src/app/generators/services/hyperparams.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Hyperparam } from '../../interfaces/hyperparam.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HyperparamsService {
+
+  private hyperparams: Hyperparam;
+  constructor() {
+    this.hyperparams = {
+      prompt: 'anime style, high quality, detailed, hair with vibrant colors, masterpiece, best quality',
+      num_inference_steps: 50,
+      num_images_per_prompt: 1,
+      strength: 0.70,
+      guidance_scale: 9.0
+    };
+  }
+
+  public getHyperparmas(): Hyperparam {
+    return this.hyperparams;
+  }
+  public setHyperParams(hyperparm: Hyperparam): void {
+    this.hyperparams = hyperparm;
+  }
+}
